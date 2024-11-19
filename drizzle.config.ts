cae173421d2c102313a424cb04b29002ca8edea1
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-11-19 01:21:57 -0500

    Fixed schema path and error check for db url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,16 @@ import 'dotenv/config'; // Load .env file
 import { defineConfig } from 'drizzle-kit'; // defineConfig is a func that returns a config object
 
 
+if (!process.env.DATABASE_URL) {
+    console.log("Cannot find database url");
+}
+
 // out defineConfig func is passed to the defineConfig func from drizzle-kit
 // Sets up drizzle environment
 // export to be used elsewhere
 export default defineConfig({
     out: './drizzle', // When migrations are run, the generated files will be placed in the drizzle folder
-    schema: './src/db/schema.ts', // The schema file that defines the database schema
+    schema: './src/app/db/schema.ts', // The schema file that defines the database schema
     dialect: 'postgresql', // Type of database
     dbCredentials: {
         url: process.env.DATABASE_URL!, // ! tells TS that the value is not null (non null assertion)
